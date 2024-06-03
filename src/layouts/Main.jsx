0a69697d705c5ed0bf08333f6cb266c3128b9e2d
mstsurnalyakter <surnalyms@gmail.com>
2024-06-03 12:16:02 +0600

    Footer section added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../pages/Shared/NavBar";
+import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
   return (
@@ -10,7 +11,7 @@ const Main = () => {
       <div className="mx-auto max-w-7xl px-7 lg:px-2 mt-10 mb-10 min-h-[calc(100vh-432px)]">
         <Outlet />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
