3d9b2238d8c66f357578402d22f97b1f9fc46718
mstsurnalyakter <surnalyms@gmail.com>
2024-06-04 06:17:24 +0600

    Tutor Section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Tutor from "../Tutor/Tutor";
 
 
 const Home = () => {
   return (
     <div className="space-y-20 dark:text-gray-100">
       <Banner/>
+      <Tutor/>
     </div>
   );
 }
---
+++ src/pages/Home/Tutor/Tutor.jsx
@@ -0,0 +1,18 @@
+import TutorCard from '../../../components/TutorCard/TutorCard'
+
+const Tutor = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <TutorCard />
+      <TutorCard />
+      <TutorCard />
+      <TutorCard />
+      <TutorCard />
+      <TutorCard />
+    </div>
+  );
+}
+
+
+
+export default Tutor
