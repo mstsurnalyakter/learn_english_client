d438c38de9dcd08038633193141d38c87211f3f1
mstsurnalyakter <surnalyms@gmail.com>
2024-06-08 15:08:43 +0600

    tutor section code updated

--- src/pages/Home/Tutor/Tutor.jsx
+++ src/pages/Home/Tutor/Tutor.jsx
@@ -23,8 +23,20 @@ const Tutor = () => {
       if (tutorsLoading) return <LoadingSpinner/>
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-      {tutors?.length > 0 && tutors?.map((user) => <TutorCard user={user} key={user?._id} />)}
+    <div className="space-y-10">
+      <div className="w-1/2 mx-auto text-center space-y-3">
+        <h2 className="text-center font-bold text-2xl">Our Tutors</h2>
+        <p>
+          100% of tutors on our platform are native English speakers. They are
+          all experts at teaching the LearnEnglish curriculum. Many hold
+          graduate degrees from top universities in the US, UK, and Canada. We
+          are continually impressed by our tutors, and we think you will be too!
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {tutors?.length > 0 &&
+          tutors?.map((user) => <TutorCard user={user} key={user?._id} />)}
+      </div>
     </div>
   );
 }
