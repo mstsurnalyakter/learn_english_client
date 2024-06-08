c6e4d624849dba45c0feb596e09a3442f025f2f4
mstsurnalyakter <surnalyms@gmail.com>
2024-06-08 15:02:32 +0600

    study session section updated

--- src/pages/Home/StudySession/StudySession.jsx
+++ src/pages/Home/StudySession/StudySession.jsx
@@ -2,28 +2,45 @@ import { useState } from "react";
 import SessionCard from "../../../components/Card/SessionCard/SessionCard"
 import useAllStudySession from "../../../hooks/useAllStudySession";
 import { Button } from "@material-tailwind/react";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosCommon from "../../../hooks/useAxiosCommon";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner/LoadingSpinner";
 
 const StudySession = () => {
    const [showAll, setShowAll] = useState(6);
-  const { studySessions, isLoading, refetch } = useAllStudySession();
   const [btn, setBtn] = useState(false);
+  const axiosCommon = useAxiosCommon();
 
-  const approvedSession = studySessions?.filter(
-    (session) => session?.status === "approved"
-  );
+
+
+
+   const {
+     data: approvedSessions = [],
+     approvedSessionLoading,
+     approvedSessionRefetch,
+   } = useQuery({
+     queryKey: ["approvedSession"],
+     queryFn: async () => {
+       const { data } = await axiosCommon(`/sessions/approved`);
+       return data;
+     },
+   });
+
+   if(approvedSessionLoading) return <LoadingSpinner/>
 
 
   return (
-    <div>
+    <div className="space-y-8">
+      <h2 className="text-center font-bold text-2xl">Discover the Study Session</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {studySessions?.length > 0 &&
-          approvedSession
+        {approvedSessions?.length > 0 &&
+          approvedSessions
             ?.slice(0, showAll)
             .map((session) => (
               <SessionCard key={session?._id} session={session} />
             ))}
       </div>
-      {approvedSession?.length > 6 && (
+      {approvedSessions?.length > 6 && (
         <div className={`text-center`}>
           {btn ? (
             <Button
@@ -39,7 +56,7 @@ const StudySession = () => {
             <Button
               className="bg-[#0073e1] capitalize"
               onClick={() => {
-                setShowAll(studySessions?.length);
+                setShowAll(approvedSessions?.length);
                 setBtn(!btn);
               }}
             >
