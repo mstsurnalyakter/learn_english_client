10b13b702797063c34799f77cd8d4d6a0abde151
mstsurnalyakter <surnalyms@gmail.com>
2024-06-08 14:43:10 +0600

    Tutor section updated

--- src/components/Card/TutorCard/TutorCard.jsx
+++ src/components/Card/TutorCard/TutorCard.jsx
@@ -1,12 +1,9 @@
 
 import PropTypes from 'prop-types'
-import { FaStar } from "react-icons/fa";
-import { MdRateReview } from "react-icons/md";
 import { PiCertificateFill } from "react-icons/pi";
+import { MdEmail } from "react-icons/md";
 
 const TutorCard = ({user}) => {
-    // const { authorName, authorImage, bio } = author || {};
-
 
     return (
       <div className="flex flex-col border-2 w-full justify-center mx-auto p-6 shadow-md rounded-xl sm:px-12">
@@ -16,27 +13,12 @@ const TutorCard = ({user}) => {
           className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square"
         />
         <div className="space-y-4 text-center divide-y dark:divide-gray-300">
-          <div className="my-2 space-y-1">
+          <div className="my-2 space-y-3">
             <h2 className="text-xl font-semibold sm:text-2xl">{user?.name}</h2>
-            <div className="flex items-center justify-center gap-6">
-              {/* kkkkkkkk */}
-              <div className="flex items-center gap-2">
-                <span className="text-orange-800">
-                  <FaStar />
-                </span>
-                4.5
-              </div>
-              {/* kkkkkkkk */}
-              <div className="flex items-center gap-2">
-                <span>
-                  <MdRateReview />
-                </span>
-                777777 reviews
-              </div>
-            </div>
-            <p className="px-5 text-xs sm:text-base dark:text-gray-600">
-              llllllllllllllll
-            </p>
+            <p className='flex items-center justify-center gap-2'>
+            <MdEmail/>
+              {user?.email}
+              </p>
 
             <div className="flex items-center justify-center gap-3 bg-[#d9e9fb] mx-auto w-48 text-[#1883fc]">
               <PiCertificateFill />
@@ -87,20 +69,6 @@ const TutorCard = ({user}) => {
                 className="w-4 h-4 fill-current"
               >
                 <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-              </svg>
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              aria-label="Email"
-              className="p-2 rounded-md dark:text-gray-800 hover:dark:text-violet-600"
-            >
-              <svg
-                viewBox="0 0 512 512"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-4 h-4 fill-current"
-              >
-                <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
               </svg>
             </a>
           </div>
--- src/pages/Home/Tutor/Tutor.jsx
+++ src/pages/Home/Tutor/Tutor.jsx
@@ -1,12 +1,27 @@
 
 
 import TutorCard from "../../../components/Card/TutorCard/TutorCard";
-import useUsers from "../../../hooks/useUsers";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner/LoadingSpinner";
+import useAxiosCommon from "../../../hooks/useAxiosCommon";
+import { useQuery } from "@tanstack/react-query";
 
 const Tutor = () => {
-  const { users, userLoading, refetch } =  useUsers();
-  const tutors = users?.filter((user) => user?.role === 'tutor');
-  console.log(tutors);
+  const axiosCommon = useAxiosCommon();
+
+      const {
+        data:tutors = [],
+        tutorsLoading,
+        tutorsRefetch,
+      } = useQuery({
+        queryKey: ["tutors"],
+        queryFn: async () => {
+          const { data } = await axiosCommon(`/tutors/tutor`);
+          return data;
+        },
+      });
+
+      if (tutorsLoading) return <LoadingSpinner/>
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
       {tutors?.length > 0 && tutors?.map((user) => <TutorCard user={user} key={user?._id} />)}
